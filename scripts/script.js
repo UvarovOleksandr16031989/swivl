146562a340701bdc6c9a285cbd5dade9ea069013
UvarovOleksandr16031989 <alexuvarov1989@ukr.net>
2019-02-01 00:02:00 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+;
+$('.nav__item').click(function() {
+    $('.nav__item').removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.users__btn').click(function () {
+   $('.report') .toggleClass('active');
+});
+
+
+
+
+
